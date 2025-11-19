acd6b8392b974a0a66b5f453bef36161ec540ebc
Andrei <45366764+Usov-github@users.noreply.github.com>
2025-11-19 20:53:28 +0500

    Update exporterSessionsData.go

--- explorers/exporterSessionsData.go
+++ explorers/exporterSessionsData.go
@@ -46,12 +46,10 @@ func (exp *ExporterSessionsMemory) Construct(s *settings.Settings) *ExporterSess
 	exp.BaseExporter = newBase(exp.GetName())
 	exp.logger.Info("Создание объекта")
 
-	labelName := s.GetMetricNamePrefix() + exp.GetName()
-
-
+	prefix := s.GetMetricNamePrefix()
 	exp.summary = prometheus.NewSummaryVec(
 		prometheus.SummaryOpts{
-			Name:        labelName,
+			Name:        prefix + exp.GetName(),
 			Help:        "Показатели сессий из кластера 1С",
 			Objectives:  map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
 			ConstLabels: prometheus.Labels{"ras_host": s.GetRASHostPort()},
@@ -61,13 +59,13 @@ func (exp *ExporterSessionsMemory) Construct(s *settings.Settings) *ExporterSess
 
 	exp.startedAtGauge = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "session_start_timestamp",
+			Name: prefix + "session_start_timestamp",
 			Help: "Timestamp when the 1C session started",
 		},
 		[]string{"host", "base", "user", "id", "appid"},
 	)
 
-	exp.buff = map[string]*sessionsData{}
+	exp.buff = make(map[string]*sessionsData)
 	exp.settings = s
 	exp.ExporterCheckSheduleJob.settings = s
 	exp.cache = expirable.NewLRU[string, []map[string]string](5, nil, time.Second*5)
@@ -87,75 +85,60 @@ func atoi(n string) int64 {
 func (exp *ExporterSessionsMemory) collectingMetrics(delay time.Duration) {
 	layout := "2006-01-02T15:04:05"
 	for {
-		ses, _ := exp.getSessions()
+		sessions, err := exp.getSessions()
+		if err != nil {
+			exp.logger.Error(err)
+			continue
+		}
 
-		for _, item := range ses {
-			appid, _ := item["app-id"]
-			user, _ := item["user-name"]
-			memorytotal, _ := item["memory-total"]
-			memorycurrent, _ := item["memory-current"]
-			readcurrent, _ := item["read-current"]
-			readtotal, _ := item["read-total"]
-			writecurrent, _ := item["write-current"]
-			writetotal, _ := item["write-total"]
-			durationcurrent, _ := item["duration-current"]
-			durationcurrentdbms, _ := item["duration-current-dbms"]
-			if durationcurrentdbms == "" {
-				durationcurrentdbms, _ = item["duration current-dbms"]
-			}
-			durationall, _ := item["duration-all"]
-			durationalldbms, _ := item["duration-all-dbms"]
-			cputimecurrent, _ := item["cpu-time-current"]
-			cputimetotal, _ := item["cpu-time-total"]
-			dbmsbytesall, _ := item["dbms-bytes-all"]
-			callsall, _ := item["calls-all"]
-			sessionid, _ := item["session-id"]
-
+		for _, item := range sessions {
+			sessionid := item["session-id"]
 			var startedAtUnix int64
 			if startedAt, err := time.Parse(layout, item["started-at"]); err == nil {
 				startedAtUnix = startedAt.Unix()
 			}
 
 			exp.mx.Lock()
-			if v, ok := exp.buff[sessionid]; !ok {
-				exp.buff[sessionid] = &sessionsData{
+			v, found := exp.buff[sessionid]
+			if !found {
+				v = &sessionsData{
 					basename:            exp.findBaseName(item["infobase"]),
-					appid:               appid,
-					user:                user,
-					memorytotal:         atoi(memorytotal),
-					memorycurrent:       atoi(memorycurrent),
-					readcurrent:         atoi(readcurrent),
-					readtotal:           atoi(readtotal),
-					writecurrent:        atoi(writecurrent),
-					writetotal:          atoi(writetotal),
-					durationcurrent:     atoi(durationcurrent),
-					durationcurrentdbms: atoi(durationcurrentdbms),
-					durationall:         atoi(durationall),
-					durationalldbms:     atoi(durationalldbms),
-					cputimecurrent:      atoi(cputimecurrent),
-					cputimetotal:        atoi(cputimetotal),
-					dbmsbytesall:        atoi(dbmsbytesall),
-					callsall:            atoi(callsall),
+					appid:               item["app-id"],
+					user:                item["user-name"],
+					memorytotal:         atoi(item["memory-total"]),
+					memorycurrent:       atoi(item["memory-current"]),
+					readcurrent:         atoi(item["read-current"]),
+					readtotal:           atoi(item["read-total"]),
+					writecurrent:        atoi(item["write-current"]),
+					writetotal:          atoi(item["write-total"]),
+					durationcurrent:     atoi(item["duration-current"]),
+					durationcurrentdbms: atoi(item["duration-current-dbms"]),
+					durationall:         atoi(item["duration-all"]),
+					durationalldbms:     atoi(item["duration-all-dbms"]),
+					cputimecurrent:      atoi(item["cpu-time-current"]),
+					cputimetotal:        atoi(item["cpu-time-total"]),
+					dbmsbytesall:        atoi(item["dbms-bytes-all"]),
+					callsall:            atoi(item["calls-all"]),
 					sessionid:           sessionid,
 					startedAt:           startedAtUnix,
 				}
-			} else {
-				v.memorycurrent = int64(math.Max(float64(v.memorycurrent), float64(atoi(memorycurrent))))
-				v.readcurrent = int64(math.Max(float64(v.readcurrent), float64(atoi(readcurrent))))
-				v.cputimecurrent = int64(math.Max(float64(v.cputimecurrent), float64(atoi(cputimecurrent))))
-				v.durationcurrentdbms = int64(math.Max(float64(v.durationcurrentdbms), float64(atoi(durationcurrentdbms))))
-				v.durationcurrent = int64(math.Max(float64(v.durationcurrent), float64(atoi(durationcurrent))))
-				v.writecurrent = int64(math.Max(float64(v.writecurrent), float64(atoi(writecurrent))))
-				v.dbmsbytesall = atoi(dbmsbytesall)
-				v.cputimetotal = atoi(cputimetotal)
-				v.durationalldbms = atoi(durationalldbms)
-				v.durationall = atoi(durationall)
-				v.writetotal = atoi(writetotal)
-				v.readtotal = atoi(readtotal)
-				v.memorytotal = atoi(memorytotal)
-				v.callsall = atoi(callsall)
-				v.startedAt = startedAtUnix
 				exp.buff[sessionid] = v
+			} else {
+				v.memorycurrent = int64(math.Max(float64(v.memorycurrent), float64(atoi(item["memory-current"]))))
+				v.readcurrent = int64(math.Max(float64(v.readcurrent), float64(atoi(item["read-current"]))))
+				v.cputimecurrent = int64(math.Max(float64(v.cputimecurrent), float64(atoi(item["cpu-time-current"]))))
+				v.durationcurrentdbms = int64(math.Max(float64(v.durationcurrentdbms), float64(atoi(item["duration-current-dbms"]))))
+				v.durationcurrent = int64(math.Max(float64(v.durationcurrent), float64(atoi(item["duration-current"]))))
+				v.writecurrent = int64(math.Max(float64(v.writecurrent), float64(atoi(item["write-current"]))))
+				v.dbmsbytesall = atoi(item["dbms-bytes-all"])
+				v.cputimetotal = atoi(item["cpu-time-total"])
+				v.durationalldbms = atoi(item["duration-all-dbms"])
+				v.durationall = atoi(item["duration-all"])
+				v.writetotal = atoi(item["writetotal"])
+				v.readtotal = atoi(item["read-total"])
+				v.memorytotal = atoi(item["memory-total"])
+				v.callsall = atoi(item["calls-all"])
+				v.startedAt = startedAtUnix
 			}
 			exp.mx.Unlock()
 		}
@@ -175,7 +158,7 @@ func (exp *ExporterSessionsMemory) getValue() {
 	defer exp.mx.Unlock()
 
 	exp.summary.Reset()
-	for k, v := range exp.buff {
+	for _, v := range exp.buff {
 		exp.summary.WithLabelValues(exp.host, v.basename, v.user, v.sessionid, "memorytotal", v.appid).Observe(float64(v.memorytotal))
 		exp.summary.WithLabelValues(exp.host, v.basename, v.user, v.sessionid, "memorycurrent", v.appid).Observe(float64(v.memorycurrent))
 		exp.summary.WithLabelValues(exp.host, v.basename, v.user, v.sessionid, "readcurrent", v.appid).Observe(float64(v.readcurrent))
@@ -192,8 +175,9 @@ func (exp *ExporterSessionsMemory) getValue() {
 		exp.summary.WithLabelValues(exp.host, v.basename, v.user, v.sessionid, "callsall", v.appid).Observe(float64(v.callsall))
 
 		exp.startedAtGauge.WithLabelValues(exp.host, v.basename, v.user, v.sessionid, v.appid).Set(float64(v.startedAt))
-		delete(exp.buff, k)
 	}
+
+	exp.buff = make(map[string]*sessionsData) // очистка буфера
 }
 
 func (exp *ExporterSessionsMemory) Collect(ch chan<- prometheus.Metric) {
